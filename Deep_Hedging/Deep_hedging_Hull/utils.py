06f089d021fd8b38e1af2a89e71bfd975e063126
Antoinebattini <112547342+Antoinebattini@users.noreply.github.com>
2025-03-07 15:04:11 +0100

    Même données

--- Deep_Hedging/Deep_hedging_Hull/utils.py
+++ Deep_Hedging/Deep_hedging_Hull/utils.py
@@ -57,13 +57,13 @@ def get_sim_path(M, freq,num_sim):
     # Annual Return
     mu = 0.1
     # Annual Volatility
-    vol = 0.3
+    vol = 0.2
     # Initial Asset Value
     S = 100
     # Option Strike Price
-    K = 100
+    K = 110
     # Annual Risk Free Rate
-    r = 0
+    r = 0.1
     # Annual Dividend
     q = 0
     # asset price 2-d array
